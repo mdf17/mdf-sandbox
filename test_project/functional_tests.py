c3056105bef44b4c82ef13d8bad169791a8f95ea
Matt Ferrante <matthew.ferrante717@gmail.com>
2017-04-09 16:39:30 -0400

    Front page HTML now generated from a template

--- test_project/functional_tests.py
+++ test_project/functional_tests.py
@@ -26,7 +26,7 @@ class NewVisitorTest(unittest.TestCase):
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         # She types "Buy peacock feathers" into a text box
-        input.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Buy peacock feathers')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
@@ -35,7 +35,10 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue( any(row.text == '1: Buy peacock feathers' for row in rows) )
+        self.assertTrue( 
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
+        )
 
         # There is still a text box inviting her to add another item. She enters
         # "Use peacock feathers to make a fly"
