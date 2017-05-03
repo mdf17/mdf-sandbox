f9282346e82138be63b8e45f3cffedd478ab415a
Matt Ferrante <matthew.ferrante717@gmail.com>
2017-05-02 20:25:07 -0400

    new URL + view for adding to existing lists. FT passes!

--- test_project/lists/tests.py
+++ test_project/lists/tests.py
@@ -3,7 +3,6 @@ from django.test import TestCase
 from django.http import HttpRequest
 
 from lists.views import home_page
-#from lists.models import Item
 from lists.models import Item, List
 
 
@@ -18,19 +17,34 @@ class HomePageTest(TestCase):
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        list_ = List.objects.create()
+        list_url = '/lists/' + str(list_.id) + '/'
+        response = self.client.get(list_url)
         self.assertTemplateUsed(response, 'list.html')
     
-    def test_displays_all_items(self):
-        list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
+    def test_displays_only_items_for_that_list(self):
+        correct_list = List.objects.create()
+        Item.objects.create(text='itemey 1', list=correct_list)
+        Item.objects.create(text='itemey 2', list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
+        
+        correct_list_url = '/lists/' + str(correct_list.id) + '/'
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        response = self.client.get(correct_list_url)
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+        self.assertNotContains(response, 'other list item 1')
+        self.assertNotContains(response, 'other list item 2')
 
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        correct_list_url = '/lists/' + str(correct_list.id) + '/'
+        response = self.client.get(correct_list_url)
+        self.assertEqual(response.context['list'], correct_list)
 
 class NewListTest(TestCase):
 
@@ -42,10 +56,11 @@ class NewListTest(TestCase):
 
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
-        self.assertRedirects(response,'/lists/the-only-list-in-the-world/')
+        new_list = List.objects.first()
+        new_list_url = '/lists/' + str(new_list.id) + '/'
+        self.assertRedirects(response, new_list_url)
 
 
-#class ItemModelTest(TestCase):
 class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
@@ -74,3 +89,36 @@ class ListAndItemModelsTest(TestCase):
         self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, 'Item the second')
         self.assertEqual(second_saved_item.list, list_)
+
+
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        correct_list_url = '/lists/' + str(correct_list.id) + '/add_item'
+
+        self.client.post( 
+                correct_list_url, 
+                data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new item for an existing list')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        correct_list_url = '/lists/' + str(correct_list.id) + '/'
+        correct_list_add_item_url = correct_list_url + 'add_item'
+
+        response = self.client.post( 
+                correct_list_add_item_url, 
+                data={'item_text': 'A new item for an existing list'}
+        )
+
+        self.assertRedirects(response, correct_list_url)
